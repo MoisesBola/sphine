f0caad04651f7c2068defd38d5db43dd69b7f918
MoisesBola <119911135+MoisesBola@users.noreply.github.com>
2022-12-06 03:42:15 -0500

    Revert "Merge branch 'main' of https://github.com/MoisesBola/sphine"
    This reverts commit ed9080e6042fa09303277c9baf810fdead113702, reversing
    changes made to e21518eb6e0576fa4baae6b635c70023a4d85f21.

--- conexion.php
+++ conexion.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 <?php
   
@@ -13,19 +12,3 @@
 ?>
 
 
-=======
-
-<?php
-  
-   $host="babar.db.elephantsql.com";
-   $port=5432;
-   $username= "grwhgxex";
-   $password= "h-F3F7G1BmY6AzpF65nyDp6Kb411iHxJ";
-   $database= "grwhgxex";
-   $conn = pg_pconnect(" host=$host dbname=$database port=$port user=$username password=$password ");
-   
-
-?>
-
-
->>>>>>> cdb95c7f6598dfe6e016eb5546bd9336af1427b9
